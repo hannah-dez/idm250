3e35af4ac68eeb04a35a2d9f3b68e0503eaee59f
hannah-dez <hd434@drexel.edu>
2025-02-18 23:14:22 -0500

    more updates
    code basics for pages

--- 404.php
+++ 404.php
@@ -1,4 +1,6 @@
 <?php get_header(); ?>
+<div class="error-message">
 <h1>404 Error</h1>
 <p>Page Not Found</p>
+</div>
 <?php get_footer(); ?>
--- footer.php
+++ footer.php
@@ -3,10 +3,7 @@
     wp_nav_menu([
         'theme_location'=>'footer-menu', //for the theme location i want to output primary menu
     ]);
-?>
-    <div>
-    <?php dynamic_sidebar('footer-widget');?>
-    </div>    
+?>  
 </footer>
     <?php wp_footer(); ?>
 </body>
